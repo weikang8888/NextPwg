865dcefe914d1ad571c2e054c6b8594f166d7236
Jinnypwg <jiayanpwg@gmail.com>
2024-11-20 15:32:35 +0800

    asd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import LoginForm from "./LoginForm";
-import Popup from "../components/Modal/page";
-import errorIcon from "../../assets/Error.png";
-import successIcon from "../../assets/Vector.png";
 
 export default function Login() {
   return (
@@ -11,8 +8,6 @@ export default function Login() {
         <h1 className="text-black text-3xl mb-10">Login Page</h1>
         <LoginForm />
       </div>
-      {/* <Popup message={"Failed"} imageSrc={errorIcon} /> */}
-      {/* <Popup message={"Success"} imageSrc={successIcon} /> */}
     </div>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
+  /* config options here */
 };
 
 export default nextConfig;
-
-module.exports = {
-	async redirects() {
-		return [
-			{
-				source: "/",
-				destination: "/login",
-				permanent: true,
-			},
-		];
-	},
-};
