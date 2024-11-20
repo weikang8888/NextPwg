ff1d141e6b94ff31bfe0597a4f53b97b460b43e6
Kheng-PWG <kheng.pwg@gmail.com>
2024-11-20 13:52:13 +0800

    redirect the site to /login by default

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
 };
 
 export default nextConfig;
+
+module.exports = {
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/login",
+				permanent: true,
+			},
+		];
+	},
+};
