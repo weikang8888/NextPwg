9acbd1227a3522964653914980604bfd8172198f
Kheng-PWG <kheng.pwg@gmail.com>
2024-11-20 16:32:09 +0800

    dsadasdasd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import LoginForm from "./LoginForm";
 
 export default function Login() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center bg-gray-100">
-      <div className="bg-white flex flex-col justify-center items-center rounded-2xl shadow-lg py-16 px-12">
-        <h1 className="text-black text-3xl mb-10">Login Page</h1>
-        <LoginForm />
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full h-screen flex justify-center items-center bg-gray-100">
+			<div className="bg-white flex flex-col justify-center items-center rounded-2xl shadow-lg py-16 px-12">
+				<h1 className="text-black text-3xl mb-10">Login Page</h1>
+				<LoginForm />
+			</div>
+		</div>
+	);
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Add your custom configuration options here if needed */
 };
 
 export default nextConfig;
