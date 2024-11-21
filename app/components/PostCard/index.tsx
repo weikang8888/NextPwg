ae9941469ee11bb1667e03b65af82ff6d0eebbf5
Master Ng <weikang0323@hotmail.com>
2024-11-21 15:07:01 +0800

    asasdasd

--- app/components/PostCard/index.tsx
+++ app/components/PostCard/index.tsx
@@ -4,64 +4,64 @@ import PWG from "../../assets/pwg.png";
 import { useRouter } from "next/navigation";
 
 interface Post {
-	id: number;
-	userId: number;
-	title: string;
-	body: string;
-	date: string;
-	tags: string[];
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+  date: string;
+  tags: string[];
 }
 
 interface PostCardProps {
-	posts: Post[];
-	onDeletePost: (postId: number) => void;
-	onEditPost: (postId: number) => void;
+  posts: Post[];
+  onDeletePost: (postId: number) => void;
+  onEditPost: (postId: number) => void;
 }
 
 const PostCard: React.FC<PostCardProps> = ({ posts, onDeletePost, onEditPost }) => {
-	const router = useRouter();
+  const router = useRouter();
 
-	const handleViewPost = (postId: number) => {
-		router.push(`/${postId}`);
-	};
+  const handleViewPost = (postId: number) => {
+    router.push(`/${postId}`);
+  };
 
-	return (
-		<div className="row grid grid-cols-3 gap-4">
-			{posts?.map((post) => (
-				<div key={post.id} className="mb-4">
-					<div className="flex flex-col justify-between rounded-lg p-4 px-8 bg-white h-full">
-						<div className="flex justify-between items-center">
-							<span className="text-xs text-primary">{new Date(post.date).toISOString().split("T")[0]}</span>
-							<Image src={PWG} alt="PWG" className="pwg-image" />
-						</div>
-						<h5 className="text-2xl break-words whitespace-pre-wrap">{post.title}</h5>
-						<div className="text-base break-words whitespace-pre-wrap max-h-[150px] overflow-hidden flex-1">
-							{post.body}
-						</div>
-						<div className="flex gap-4 mt-2 flex-wrap">
-							{post.tags.map((tag, index) => (
-								<span key={index} className="rounded-lg p-[5px] px-[9px] text-sm bg-secondary">
-									{tag}
-								</span>
-							))}
-						</div>
-						<div className="gap-4 flex justify-between mt-4">
-							<button className="w-full rounded-3xl p-[4px] px-[10px] bg-mint" onClick={() => onEditPost(post.id)}>
-								Edit
-							</button>
-							<button
-								className="w-full rounded-3xl p-[4px] px-[10px] bg-primary"
-								onClick={() => handleViewPost(post.id)}>
-								View
-							</button>
-							<button className="w-full rounded-3xl p-[4px] px-[10px] bg-danger" onClick={() => onDeletePost(post.id)}>
-								Delete
-							</button>
-						</div>
-					</div>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="row grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {posts?.map((post) => (
+        <div key={post.id} className="mb-4">
+          <div className="flex flex-col justify-between rounded-lg p-4 px-8 bg-white h-full">
+            <div className="flex justify-between items-center">
+              <span className="text-xs text-primary">{new Date(post.date).toISOString().split("T")[0]}</span>
+              <Image src={PWG} alt="PWG" className="pwg-image" />
+            </div>
+            <h5 className="text-2xl break-words whitespace-pre-wrap">{post.title}</h5>
+            <div className="text-base break-words whitespace-pre-wrap max-h-[150px] overflow-hidden flex-1">
+              {post.body}
+            </div>
+            <div className="flex gap-4 mt-2 flex-wrap">
+              {post.tags.map((tag, index) => (
+                <span key={index} className="rounded-lg p-[5px] px-[9px] text-sm bg-secondary">
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <div className="gap-4 flex justify-between mt-4">
+              <button className="w-full rounded-3xl p-[4px] px-[10px] bg-mint" onClick={() => onEditPost(post.id)}>
+                Edit
+              </button>
+              <button
+                className="w-full rounded-3xl p-[4px] px-[10px] bg-primary"
+                onClick={() => handleViewPost(post.id)}>
+                View
+              </button>
+              <button className="w-full rounded-3xl p-[4px] px-[10px] bg-danger" onClick={() => onDeletePost(post.id)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default PostCard;
--- app/page.tsx
+++ app/page.tsx
@@ -9,156 +9,161 @@ import { jwtDecode } from "jwt-decode";
 import AddEditPostModal from "./components/AddEditPost.tsx/AddEditPostModal";
 
 interface Post {
-	id: number;
-	userId: number;
-	title: string;
-	body: string;
-	date: string;
-	tags: string[];
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+  date: string;
+  tags: string[];
 }
 
 export default function Home() {
-	const [userPosts, setUserPosts] = useState<Post[]>([]);
-	const [totalPosts, setTotalPosts] = useState<number>(0);
-	const [myTotalPosts, setMyTotalPosts] = useState<number>(0);
-	const [totalAccounts, setTotalAccounts] = useState<number>(0);
-	const [role, setRole] = useState<string>("");
-	const [loading, setLoading] = useState<boolean>(false);
-	const [showPopUp, setShowPopUp] = useState<boolean>(false);
-	const [error, setError] = useState<string | null>(null);
-	const [postId, setPostId] = useState<number | null>(null);
-	const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  const [userPosts, setUserPosts] = useState<Post[]>([]);
+  const [totalPosts, setTotalPosts] = useState<number>(0);
+  const [myTotalPosts, setMyTotalPosts] = useState<number>(0);
+  const [totalAccounts, setTotalAccounts] = useState<number>(0);
+  const [role, setRole] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [showPopUp, setShowPopUp] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [postId, setPostId] = useState<number | null>(null);
+  const [selectedPost, setSelectedPost] = useState<Post | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [limit] = useState<number>(10);
 
-	const router = useRouter();
+  const router = useRouter();
+  const token = localStorage.getItem("token");
 
-	const token = localStorage.getItem("token");
-	const page = 1;
-	const limit = 10;
+  useEffect(() => {
+    if (!token) {
+      router.push("/login");
+      return;
+    }
 
-	useEffect(() => {
-		const token = localStorage.getItem("token");
+    const fetchPosts = async () => {
+      try {
+        const decodedToken: any = jwtDecode(token);
+        setRole(decodedToken.role);
 
-		if (!token) {
-			router.push("/login");
-			return;
-		}
+        const userPostsResponse = await getUserPosts(token, currentPage, limit);
+        setUserPosts(userPostsResponse.data || []);
+        setMyTotalPosts(userPostsResponse.totalPosts || 0);
 
-		const fetchPosts = async () => {
-			try {
-				// Decode token to get user role
-				const decodedToken: any = jwtDecode(token);
-				setRole(decodedToken.role);
+        if (decodedToken.role === "admin") {
+          const allPostsResponse = await getAllPosts(token, currentPage, limit);
+          setTotalPosts(allPostsResponse.totalPosts || 0);
 
-				// Fetch user posts
-				const page = 1;
-				const limit = 10;
-				const userPostsResponse = await getUserPosts(token, page, limit);
-				setUserPosts(userPostsResponse.data || []);
-				setMyTotalPosts(userPostsResponse.totalPosts || 0);
+          const allAccountsResponse = await getAllAccounts(token);
+          setTotalAccounts(allAccountsResponse.accounts.length || 0);
+        }
+      } catch (err: any) {
+        setError(err.message || "Failed to fetch data");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-				// If the user is admin, fetch additional data
-				if (decodedToken.role === "admin") {
-					const allPostsResponse = await getAllPosts(token, page, limit);
-					setTotalPosts(allPostsResponse.totalPosts || 0);
+    fetchPosts();
+  }, [router, token, currentPage]);
 
-					const allAccountsResponse = await getAllAccounts(token);
-					setTotalAccounts(allAccountsResponse.accounts.length || 0);
-				}
-			} catch (err: any) {
-				setError(err.message || "Failed to fetch data");
-			} finally {
-				setLoading(false);
-			}
-		};
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
-		fetchPosts();
-	}, [router]);
+  const renderPaginationButtons = () => {
+    const totalPages = Math.ceil(myTotalPosts / limit);
 
-	const fetchLatestPosts = async () => {
-		try {
-			if (token) {
-				const userPostsResponse = await getUserPosts(token, page, limit);
-				setUserPosts(userPostsResponse.data || []);
-				setMyTotalPosts(userPostsResponse.totalPosts || 0);
-			}
-		} catch (error: any) {
-			console.error("Failed to fetch updated posts:", error.message);
-		}
-	};
+    return (
+      <div className="pagination-container flex justify-center p-5">
+        {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+          <button
+            key={page}
+            className={`pagination-button rounded-md p-[5px] px-[9px] ms-3 ${
+              currentPage === page ? "bg-primary text-white" : "bg-white"
+            }`}
+            onClick={() => handlePageChange(page)}>
+            {page}
+          </button>
+        ))}
+      </div>
+    );
+  };
 
-	const handleDeletePost = async (postId: number) => {
-		try {
-			await deletePost(postId);
-			setUserPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
-			setMyTotalPosts((prev) => prev - 1);
-			if (role === "admin") {
-				setTotalPosts((prev) => prev - 1);
-			}
-		} catch (error: any) {
-			alert(error.error || "Failed to delete post");
-		}
-	};
+  const handleDeletePost = async (postId: number) => {
+    try {
+      await deletePost(postId);
+      setUserPosts((prevPosts) => prevPosts.filter((post) => post.id !== postId));
+      setMyTotalPosts((prev) => prev - 1);
+      if (role === "admin") {
+        setTotalPosts((prev) => prev - 1);
+      }
+    } catch (error: any) {
+      alert(error.error || "Failed to delete post");
+    }
+  };
 
-	if (loading) {
-		return <div>Loading...</div>;
-	}
+  const handleClickAdd = () => {
+    setShowPopUp(true);
+  };
 
-	const handleClickAdd = () => {
-		setShowPopUp(true);
-	};
+  const handleClose = () => {
+    setShowPopUp(false);
+  };
 
-	const handleClose = () => {
-		setShowPopUp(false);
-	};
+  const handleClickEdit = (postId: number) => {
+    const postToEdit = userPosts.find((post) => post.id === postId);
+    if (postToEdit) {
+      setSelectedPost(postToEdit);
+      setPostId(postId);
+      setShowPopUp(true);
+    }
+  };
 
-	const handleClickEdit = (postId: number) => {
-		const postToEdit = userPosts.find((post) => post.id === postId);
-		if (postToEdit) {
-			setSelectedPost(postToEdit);
-			setPostId(postId);
-			setShowPopUp(true);
-		}
-	};
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-	return (
-		<div className="container mx-auto px-4">
-			<header className="flex justify-between items-center py-5">
-				<button type="button" className="bg-primary px-4 py-2 rounded-3xl" onClick={handleClickAdd}>
-					Add New Post
-				</button>
-				<span
-					className="text-danger cursor-pointer"
-					onClick={() => {
-						localStorage.removeItem("token");
-						router.push("/login");
-					}}>
-					Logout
-				</span>
-			</header>
+  return (
+    <div className="container mx-auto px-4">
+      <header className="flex justify-between items-center py-5">
+        <button type="button" className="bg-primary px-4 py-2 rounded-3xl" onClick={handleClickAdd}>
+          Add New Post
+        </button>
+        <span
+          className="text-danger cursor-pointer"
+          onClick={() => {
+            localStorage.removeItem("token");
+            router.push("/login");
+          }}>
+          Logout
+        </span>
+      </header>
 
-			<div className="text-center mt-3 mb-4 text-4xl">Post List</div>
+      <div className="text-center mt-3 mb-4 text-4xl">Post List</div>
 
-			{role === "admin" && (
-				<TotalPost myTotalPosts={myTotalPosts} totalAccount={totalAccounts} totalPosts={totalPosts} />
-			)}
+      {role === "admin" && (
+        <TotalPost myTotalPosts={myTotalPosts} totalAccount={totalAccounts} totalPosts={totalPosts} />
+      )}
 
-			<PostCard posts={userPosts} onDeletePost={handleDeletePost} onEditPost={handleClickEdit} />
+      <PostCard posts={userPosts} onDeletePost={handleDeletePost} onEditPost={handleClickEdit} />
 
-			<div className="flex justify-center p-5">
-				<button className="rounded-md p-[5px] px-[9px] ms-3 bg-primary">1</button>
-			</div>
+      {renderPaginationButtons()}
 
-			<AddEditPostModal
-				onClose={handleClose}
-				initialValues={
-					selectedPost
-						? { title: selectedPost.title, body: selectedPost.body, tags: selectedPost.tags }
-						: { title: "", body: "", tags: [] }
-				}
-				showPopup={showPopUp}
-				postId={postId}
-				onUpdatePosts={fetchLatestPosts}
-			/>
-		</div>
-	);
+      <AddEditPostModal
+        onClose={handleClose}
+        initialValues={
+          selectedPost
+            ? {
+                title: selectedPost.title,
+                body: selectedPost.body,
+                tags: selectedPost.tags,
+              }
+            : { title: "", body: "", tags: [] }
+        }
+        showPopup={showPopUp}
+        postId={postId}
+        onUpdatePosts={() => handlePageChange(currentPage)}
+      />
+    </div>
+  );
 }
